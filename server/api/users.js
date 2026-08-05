12911cbc78a19047a0cf61357a9363fe3781cd66
Alex Chen <achen2291@gmail.com>
2026-08-04 23:35:18 -0700

    Users router

--- server/api/users.js
+++ server/api/users.js
@@ -1,8 +1,31 @@
 import express from "express";
 const usersRouter = express.Router();
-
-usersRouter.get("/", async (req, res, next) => {
-  res.send("inside of GET /api/users route!");
-});
-
 export default usersRouter;
+
+import { createUser, getUser, getUserById } from "../db/queries/users";
+import { createToken } from "../utils/jwt";
+
+usersRouter.post(
+  "/register",
+  requireBody(["username", "password"]),
+  async (req, res) => {
+    const { username, password } = req.body;
+    const user = await createUser(username, password);
+    const token = createToken({ id: user.id });
+    res.status(201).send(token);
+  },
+);
+
+usersRouter.post(
+  "/login",
+  requireBody(["username", "password"]),
+  async (req, res) => {
+    const { usernmae, password } = req.body;
+    const user = await getUser(username, password);
+    if (!user) {
+      return res.status(401).send("Invalid username/password.");
+    }
+    const token = createToken({ id: user.id });
+    res.send(token);
+  },
+);
